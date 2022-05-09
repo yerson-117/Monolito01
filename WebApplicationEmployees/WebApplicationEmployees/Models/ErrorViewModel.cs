632285fef3f00be730211505a628758c1576d389
yerson-117 <yjaragon1@misena.edu.co>
2022-05-09 14:05:37 -0500

    plantilla del repositorio

---
+++ WebApplicationEmployees/WebApplicationEmployees/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationEmployees.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
